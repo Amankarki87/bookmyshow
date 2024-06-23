fb87016408d883f9f31c5b53275ee1097e1f4f2a
Amankarki87 <amankarki87@gmail.com>
2024-06-23 10:51:22 +0545

    Remove commented code

--- src/test/java/bookMyShow/controllers/MovieControllerTest.java
+++ src/test/java/bookMyShow/controllers/MovieControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest
 @WebMvcTest(value=MovieController.class)
 @ContextConfiguration(classes = {MovieController.class})
 public class MovieControllerTest {
